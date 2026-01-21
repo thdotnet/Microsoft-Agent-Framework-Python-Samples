fb54fa7de415e748556410acee3b919488ec0ac1
Thiago Custodio <thxplode@gmail.com>
2026-01-21 17:11:11 -0600

    Rename echo function to fabric

--- http-tool.py
+++ http-tool.py
@@ -6,7 +6,7 @@ from fabric_data_agent_client import FabricDataAgentClient
 app = FastAPI()
 
 @app.get("/fabric")
-def echo(text: str = Query(..., min_length=1, description="query to ask Fabric Data Agent")):
+def fabric(text: str = Query(..., min_length=1, description="query to ask Fabric Data Agent")):
     client = FabricDataAgentClient(
         tenant_id="",
         data_agent_url="https://api.fabric.microsoft.com/v1/workspaces/{workspaceid}/dataagents/{dataagentid}/aiassistant/openai"
