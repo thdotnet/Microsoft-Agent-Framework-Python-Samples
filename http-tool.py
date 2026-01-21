1bc2b9fb39c0e23348b7201b6fc5261e97d7660a
Thiago Custodio <thxplode@gmail.com>
2026-01-21 16:59:48 -0600

    Implement FastAPI application with Fabric Data Agent

---
+++ http-tool.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Query
+
+from fabric_data_agent_client import FabricDataAgentClient
+from fabric_data_agent_client import FabricDataAgentClient
+
+app = FastAPI()
+
+@app.get("/fabric")
+def echo(text: str = Query(..., min_length=1, description="query to ask Fabric Data Agent")):
+    client = FabricDataAgentClient(
+        tenant_id="",
+        data_agent_url="https://api.fabric.microsoft.com/v1/workspaces/{workspaceid}/dataagents/{dataagentids}/aiassistant/openai"
+    )
+
+    # Ask a simple question
+    response = client.ask(text)
+    return {"response": response}
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
